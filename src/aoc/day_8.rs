18c47e1b810ae9d24bb44131ebdd5b44168b328d
dropfred <dropfred@github.com>
2024-12-17 11:47:22 +0100

    day 8 - part 2

--- src/aoc/day_8.rs
+++ src/aoc/day_8.rs
@@ -13,40 +13,67 @@ impl Data {
 fn part_1(data: &Data) -> u32 {
     let width = data.grid[0].len() as i32;
     let height = data.grid.len() as i32;
-    let mut nodes = HashMap::new();
+    let mut antennas = HashMap::new();
     let mut anti_nodes = HashSet::new();
-    let mut count = 0u32;
     for y in 0..height {
         for x in 0..width {
             let c = data.grid[y as usize][x as usize];
             if c != '.' {
-                let locs = nodes.entry(c).or_insert(Vec::new());
-                for (lx, ly) in locs.iter() {
+                let antennas = antennas.entry(c).or_insert(Vec::new());
+                for (lx, ly) in antennas.iter() {
                     let (dx, dy) = (x - lx, y - ly);
                     let (nx, ny) = (lx - dx, ly - dy);
                     if (nx >= 0) && (nx < width) && (ny >= 0) && (ny < height) {
-                        if !anti_nodes.contains(&(nx, ny)) {
-                            anti_nodes.insert((nx, ny));
-                            count += 1;
-                        }
+                        anti_nodes.insert((nx, ny));
                     }
                     let (nx, ny) = (x + dx, y + dy);
                     if (nx >= 0) && (nx < width) && (ny >= 0) && (ny < height) {
-                        if !anti_nodes.contains(&(nx, ny)) {
-                            anti_nodes.insert((nx, ny));
-                            count += 1;
-                        }
+                        anti_nodes.insert((nx, ny));
                     }
                 }
-                locs.push((x, y));
+                antennas.push((x, y));
             }
         }
     }
-    count
+    anti_nodes.len() as u32
 }
 
 fn part_2(data: &Data) -> u32 {
-    todo!("part 2");
+    let width = data.grid[0].len() as i32;
+    let height = data.grid.len() as i32;
+    let size = std::cmp::max(width, height);
+    let mut antennas = HashMap::new();
+    let mut anti_nodes = HashSet::new();
+    for y in 0..height {
+        for x in 0..width {
+            let c = data.grid[y as usize][x as usize];
+            if c != '.' {
+                let antennas = antennas.entry(c).or_insert(Vec::new());
+                for (lx, ly) in antennas.iter() {
+                    let (dx, dy) = (x - lx, y - ly);
+                    for i in 0..size {
+                        let (nx, ny) = (lx - dx * i, ly - dy * i);
+                        let d1 = if (nx >= 0) && (nx < width) && (ny >= 0) && (ny < height) {
+                            anti_nodes.insert((nx, ny));
+                            false
+                        } else {
+                            true
+                        };
+                        let (nx, ny) = (x + dx * i, y + dy * i);
+                        let d2 = if (nx >= 0) && (nx < width) && (ny >= 0) && (ny < height) {
+                            anti_nodes.insert((nx, ny));
+                            false
+                        } else {
+                            true
+                        };
+                        if d1 && d2 {break;}
+                    }
+                }
+                antennas.push((x, y));
+            }
+        }
+    }
+    anti_nodes.len() as u32
 }
 
 pub fn solve() {
@@ -77,6 +104,6 @@ mod tests {
     fn test_part_2() {
         let data = include_str!("../../data/day_8/test.txt");
         let data = Data::new(data);
-        assert!(part_2(&data) == 14);
+        assert!(part_2(&data) == 34);
     }
 }
