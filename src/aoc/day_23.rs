9331e53499abd9a201bc2e21db6e1909f8e7cc3e
dropfred <dropfred@github.com>
2025-01-04 17:06:41 +0100

    day 23 - part 1

--- src/aoc/day_23.rs
+++ src/aoc/day_23.rs
@@ -1,9 +1,26 @@
+use std::collections::{HashMap, HashSet};
+
 struct Puzzle {
+    connections: Vec<(u16, u16)>
 }
 
 impl Puzzle {
     fn parse(data: &str) -> Option<Self> {
-        None
+        let parse_connection = |s: &str| {
+            let (a, b) = s.trim().split_once("-")?;
+            let (a, b) = (a.trim(), b.trim());
+            if a.is_ascii() && (a.len() == 2) && b.is_ascii() && (b.len() == 2) {
+                let (a, b) = (a.as_bytes(), b.as_bytes());
+                let a= (a[1] as u16) | ((a[0] as u16) << 8);
+                let b= (b[1] as u16) | ((b[0] as u16) << 8);
+                if a < b {Some((a, b))} else {Some((b, a))}
+            } else {
+                None
+            }
+        };
+        let connections: Option<Vec<_>> = data.trim().lines().map(parse_connection).collect();
+        let connections = connections?;
+        Some(Self {connections})
     }
 
     fn load(data: &str) -> Self {
@@ -11,8 +28,43 @@ impl Puzzle {
     }
 }
 
+fn get_trios(puzzle: &Puzzle) -> Vec<(u16, u16, u16)> {
+    let mut connections: HashMap<_, _> = HashMap::new();
+    for (a, b) in &puzzle.connections {
+        let c = connections.entry(*a).or_insert(HashSet::new());
+        c.insert(*b);
+    }
+
+    let mut ts = Vec::new();
+    for (a, bs) in &connections {
+        for b in bs {
+            if let Some(cs) = connections.get(b) {
+                for c in cs {
+                    if bs.contains(c) {
+                        ts.push((*a, *b, *c));
+                    }
+                }
+            }
+        }
+    };
+    ts.sort();
+    ts
+}
+
+// fn print_computer(computer: u16) {
+//     let (a, b) = (computer & 0xff, computer >> 8);
+//     let (a, b) = (a as u8  as char, b as u8 as char);
+//     print!("{b}{a}");
+// }
+
 fn part_1(puzzle: &Puzzle) -> usize {
-    todo!("part 1");
+    let ts = get_trios(puzzle);
+    let ts = ts.into_iter().filter(|(a, b, c)| {
+        let t = 't' as u8;
+        (((a >> 8) as u8) == t) || (((b >> 8) as u8) == t)  || (((c >> 8) as u8) == t)
+    }).count();
+
+    ts
 }
 
 fn part_2(puzzle: &Puzzle) -> usize {
@@ -20,9 +72,10 @@ fn part_2(puzzle: &Puzzle) -> usize {
 }
 
 pub fn solve() {
-    let puzzle = Puzzle {};
-    part_1(&puzzle);
-    part_2(&puzzle);
+    let data = include_str!("../../data/day_23/input.txt");
+    let puzzle = Puzzle::load(data);
+    println!("part 1: {}", part_1(&puzzle));
+    println!("part 2: {}", part_2(&puzzle));
 }
 
 #[cfg(test)]
@@ -31,15 +84,24 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let data = "
-        ab-cd
-        ";
+        let data = "ab-cd";
         let puzzle = Puzzle::parse(data);
-        assert
+        assert!(puzzle.is_some());
+
+        let data = "ab=cd";
+        let puzzle = Puzzle::parse(data);
+        assert!(puzzle.is_none());
+
+        let data = "ab-cd-ef";
+        let puzzle = Puzzle::parse(data);
+        assert!(puzzle.is_none());
     }
 
     #[test]
     fn test_part_1() {
+        let data = include_str!("../../data/day_23/test.txt");
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_1(&puzzle), 7);
     }
 
     #[test]
