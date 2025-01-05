d8cfa93417b6eaa80583882bf3c1f4671fd9ea28
dropfred <dropfred@github.com>
2025-01-05 13:51:28 +0100

    day 1 cleanup

--- src/aoc/day_1.rs
+++ src/aoc/day_1.rs
@@ -1,40 +1,49 @@
 use std::collections::HashMap;
 
-struct Data(Vec<u32>, Vec<u32>);
+struct Puzzle {
+    locations: Vec<(u32, u32)>
+}
 
-impl Data {
-    fn new(data: &str) -> Self {
-        let mut v0 : Vec<u32> = Vec::new();
-        let mut v1 : Vec<u32> = Vec::new();
-        for line in data.lines() {
-            let mut vs = line.split_ascii_whitespace();
-            v0.push(vs.next().unwrap().parse::<u32>().unwrap());
-            v1.push(vs.next().unwrap().parse::<u32>().unwrap());
-        }
-        Data(v0, v1)
+impl Puzzle {
+    fn parse(data: &str) -> Option<Self> {
+        let parse_locations = |s: &str| {
+            let mut vs = s.split_ascii_whitespace();
+            let v0 = vs.next()?.parse::<u32>().ok()?;
+            let v1 = vs.next()?.parse::<u32>().ok()?;
+            if vs.next().is_some() {return None;}
+            Some((v0, v1))
+        };
+        let locations: Option<Vec<_>> = data.trim().lines().map(parse_locations).collect();
+        let locations = locations?;
+        Some(Puzzle {locations})
+    }
+
+    fn load(data: &str) -> Self {
+        Self::parse(data).expect("valid input")
     }
 }
 
-fn part_1(data: &Data) -> u32 {
-    let (mut v0, mut v1) = (data.0.clone(), data.1.clone());
-    v0.sort();
-    v1.sort();
-
-    v0.iter().zip(v1.iter()).map(|(v1, v2)| if v1 > v2 {v1 - v2} else {v2 - v1}).sum()
+fn part_1(puzzle: &Puzzle) -> u32 {
+    let mut v0s = Vec::new();
+    let mut v1s = Vec::new();
+    for (v0, v1) in &puzzle.locations {
+        v0s.push(v0);
+        v1s.push(v1);
+    };
+    v0s.sort();
+    v1s.sort();
+    v0s.into_iter().zip(v1s.into_iter()).map(|(v1, v2)| if v1 > v2 {v1 - v2} else {v2 - v1}).sum()
 }
 
-fn part_2(data: &Data) -> u32 {
-    let (v0, v1) = (&data.0, &data.1);
-
+fn part_2(puzzle: &Puzzle) -> u32 {
     let mut v2 : HashMap<u32, u32> = HashMap::new();
-    for v in v1 {
-        *v2.entry(*v).or_insert(0) += 1;
+    for (_, v1) in &puzzle.locations {
+        *v2.entry(*v1).or_insert(0) += 1;
     }
-
     let mut distance = 0;
-    for v in v0 {
-        if v2.contains_key(v) {
-            distance += v2[&v] * v;
+    for (v0, _) in &puzzle.locations {
+        if v2.contains_key(v0) {
+            distance += v2[&v0] * v0;
         }
     }
     distance
@@ -42,9 +51,9 @@ fn part_2(data: &Data) -> u32 {
 
 pub fn solve() {
     let data = include_str!("../../data/day_1/input.txt");
-    let data = Data::new(data);
-    println!("part 1: {}", part_1(&data));
-    println!("part 2: {}", part_2(&data));
+    let puzzle = Puzzle::load(data);
+    println!("part 1: {}", part_1(&puzzle));
+    println!("part 2: {}", part_2(&puzzle));
 }
 
 #[cfg(test)]
@@ -54,14 +63,14 @@ mod tests {
     #[test]
     fn test_part_1() {
         let data = include_str!("../../data/day_1/test.txt");
-        let data = Data::new(data);
-        assert_eq!(part_1(&data), 11);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_1(&puzzle), 11);
     }
 
     #[test]
     fn test_part_2() {
         let data = include_str!("../../data/day_1/test.txt");
-        let data = Data::new(data);
-        assert_eq!(part_2(&data), 31);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_2(&puzzle), 31);
     }
 }
