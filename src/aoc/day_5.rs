d24ffbb2fbcd9c27d466c24a5fff7591ec179a38
dropfred <dropfred@github.com>
2025-01-05 19:56:46 +0100

    day 5 cleanup

--- src/aoc/day_5.rs
+++ src/aoc/day_5.rs
@@ -1,75 +1,74 @@
 use std::{cmp::Ordering, collections::HashMap};
 
-struct Data {
+struct Puzzle {
     rules: HashMap<u32, Vec<u32>>,
     updates: Vec<Vec<u32>>
 }
 
-impl Data {
-    fn new(data: &str) -> Self {
-        let data: Vec<&str> = data.lines().collect();
-        let data = {
-            let s = data.iter().position(|s| s.is_empty()).unwrap();
+impl Puzzle {
+    fn parse(data: &str) -> Option <Self> {
+        let data: Vec<_> = data.trim().lines().map(|s| s.trim()).collect();
+        let (order, updates) = {
+            let s = data.iter().position(|s| s.is_empty())?;
             (data[..s].iter(), data[(s + 1)..].iter())
         };
 
-        let mut rules : HashMap<u32, Vec<u32>> = HashMap::new();
-        for update in data.0 {
-            let s = update.find('|').unwrap();
-            let (p1, p2) = (update[..s].parse::<u32>().unwrap(), update[(s + 1)..].parse::<u32>().unwrap());
+        let mut rules = HashMap::new();
+        for update in order {
+            let (p1, p2) = update.split_once('|')?;
+            let (p1, p2) = (p1.parse().ok()?, p2.parse().ok()?);
             rules.entry(p1).or_insert(Vec::new()).push(p2);
         }
 
-        let updates: Vec<_> = data.1.map(|pages| {
-            pages.split(',').map(|page| page.parse::<u32>().unwrap()).collect::<Vec<_>>()
-        }).collect();
-        // assert!(updates.iter().all(|u| (u.len() & 1) != 0));
+        let parse_update = |ps: &&str| {
+            let ps: Option<Vec<_>> = ps.split(',').map(|p| p.parse().ok()).collect();
+            Some(ps?)
+        };
+        let updates: Option<Vec<_>> = updates.map(parse_update).collect();
+        let updates = updates?;
 
-        Data {rules, updates}
+        Some(Puzzle {rules, updates})
+    }
+
+    fn load(data: &str) -> Self {
+        Self::parse(data).expect("valid input")
     }
 }
 
 fn is_valid_update(update: &Vec<u32>, rules: &HashMap<u32, Vec<u32>>) -> bool {
-    for (i, p) in update.iter().enumerate() {
-        if rules.get(p).is_some_and(|rs| {rs.iter().any(|p| update[..i].contains(p))}) {
-            return false;
-        }
-    }
-    true
+    update.iter().enumerate().all(|(i, p)| {
+        !rules.get(p).is_some_and(|rs| {rs.iter().any(|p| update[..i].contains(p))})
+    })
 }
 
-fn part_1(data: &Data) -> u32 {
-    let mut total = 0;
-    for update in &data.updates {
-        if is_valid_update(update, &data.rules) {
-            total += update[update.len() / 2];
-        }
-    }
-    total
+fn part_1(data: &Puzzle) -> u32 {
+    data.updates.iter()
+        .filter(|u| is_valid_update(u, &data.rules))
+        .map(|u| u[u.len() / 2])
+        .sum()
 }
 
 
-fn part_2(data: &Data) -> u32 {
-    let mut total = 0;
-    for update in &data.updates {
-        if !is_valid_update(update, &data.rules) {
-            let mut update = update.clone();
-            update.sort_by(|p1, p2| {
-                if      data.rules.get(p1).is_some_and(|rs| {rs.contains(p2)}) {Ordering::Less}
-                else if data.rules.get(p2).is_some_and(|rs| {rs.contains(p1)}) {Ordering::Greater}
+fn part_2(data: &Puzzle) -> u32 {
+    data.updates.iter()
+        .filter(|u| !is_valid_update(u, &data.rules))
+        .map(|u| {
+            let mut u = u.clone();
+            u.sort_by(|p1, p2| {
+                if      data.rules.get(p1).is_some_and(|ps| {ps.contains(p2)}) {Ordering::Less}
+                else if data.rules.get(p2).is_some_and(|ps| {ps.contains(p1)}) {Ordering::Greater}
                 else {Ordering::Equal}
             });
-            total += update[update.len() / 2];
-        }
-    }
-    total
+            u[u.len() / 2]
+        })
+        .sum()
 }
 
 pub fn solve() {
     let data = include_str!("../../data/day_5/input.txt"); // 6951 / 4121
-    let data = Data::new(data);
-    println!("part 1: {}", part_1(&data));
-    println!("part 2: {}", part_2(&data));
+    let puzzle = Puzzle::load(data);
+    println!("part 1: {}", part_1(&puzzle));
+    println!("part 2: {}", part_2(&puzzle));
 }
 
 #[cfg(test)]
@@ -79,23 +78,23 @@ mod tests {
     #[test]
     fn test_data() {
         let data = include_str!("../../data/day_5/test.txt");
-        let data = Data::new(data);
-        assert_eq!(data.updates.len(), 6);
-        assert!(data.updates.iter().all(|u| (u.len() & 1) != 0));
-        assert_eq!(data.rules.len(), 6);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(puzzle.updates.len(), 6);
+        assert!(puzzle.updates.iter().all(|u| (u.len() & 1) != 0));
+        assert_eq!(puzzle.rules.len(), 6);
     }
 
     #[test]
     fn test_part_1() {
         let data = include_str!("../../data/day_5/test.txt");
-        let data = Data::new(data);
-        assert_eq!(part_1(&data), 143);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_1(&puzzle), 143);
     }
 
     #[test]
     fn test_part_2() {
         let data = include_str!("../../data/day_5/test.txt");
-        let data = Data::new(data);
-        assert_eq!(part_2(&data), 123);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_2(&puzzle), 123);
     }
 }
