65bf79b79c40a41bd213452acca6fb9e1859c81f
dropfred <dropfred@github.com>
2025-01-13 15:13:17 +0100

    day 15 cleanup

--- src/aoc/day_15.rs
+++ src/aoc/day_15.rs
@@ -1,58 +1,31 @@
-struct Data {
-    grid: Vec<Vec<char>>,
+use crate::aoc::grid::Grid;
+
+struct Puzzle {
+    map: Grid<char>,
     moves: Vec<char>
 }
 
-impl Data {
-    fn parse(data: &str) -> Self {
+impl Puzzle {
+    fn parse(data: &str) -> Option<Self> {
         let data = data.trim().replace("\r", "");
-        let (grid, moves) = data.split_once("\n\n").unwrap();
-        let grid = grid.lines().map(|s| {
-            s.trim().chars().collect()
-        }).collect();
+        let (map, moves) = data.split_once("\n\n")?;
+        let map = Grid::parse(map, "")?;
         let moves = moves.lines().map(|s| s.trim()).collect::<String>().chars().collect();
-        Data {grid, moves}
+        Some(Puzzle {map, moves})
     }
 
-    // fn get(&self, x: i32, y: i32) -> char {
-    //     self.grid[y as usize][x as usize]
-    // }
-
-    // fn get_mut(&mut self, x: i32, y: i32) -> &mut char {
-    //     &mut self.grid[y as usize][x as usize]
-    // }
-
-    fn size(&self) -> (i32, i32) {
-        (self.grid[0].len() as i32, self.grid.len() as i32)
+    fn load(data: &str) -> Self {
+        Self::parse(data).expect("valid input")
     }
 }
 
-fn get_tile(grid: &Vec<Vec<char>>, x: i32, y: i32) -> char {
-    grid[y as usize][x as usize]
-}
+fn part_1(puzzle: &Puzzle) -> u32 {
+    let mut map = puzzle.map.clone();
 
-fn set_tile(grid: &mut Vec<Vec<char>>, x: i32, y: i32, c: char) {
-    grid[y as usize][x as usize] = c;
-}
+    let (x, y) = puzzle.map.find('@').expect("robot");
+    let (mut x, mut y) = (x as i32, y as i32);
 
-fn find_robot(data: &Data) -> Option<(i32, i32)> {
-    let (w, h) = data.size();
-    for y in 1..(h - 1) {
-        for x in 1..(w - 1) {
-            if get_tile(&data.grid, x, y) == '@' {
-                return Some((x, y));
-            }
-        }
-    }
-    None
-}
-
-fn part_1(data: &Data) -> u32 {
-    let mut grid = data.grid.clone();
-
-    let (mut x, mut y) = find_robot(&data).expect("robot shoud be present");
-
-    for m in &data.moves {
+    for m in &puzzle.moves {
         let (dx, dy) = match m {
             '<' => (-1, 0),
             '>' => (1, 0),
@@ -61,28 +34,28 @@ fn part_1(data: &Data) -> u32 {
             _ => panic!("invalid move")
         };
         let (mut nx, mut ny) = (x + dx, y + dy);
-        while get_tile(&grid, nx, ny) == 'O' {
+        while map.get((nx as usize, ny as usize)) == 'O' {
             nx += dx;
             ny += dy;
         }
-        if get_tile(&grid, nx, ny) == '.' {
+        if map.get((nx as usize, ny as usize)) == '.' {
             let d = (nx - x).abs() + (ny - y).abs();
             for _ in 0..d {
-                let c = get_tile(&grid, nx - dx, ny - dy);
-                set_tile(&mut grid, nx, ny, c);
+                let c = map.get(((nx - dx) as usize, (ny - dy) as usize));
+                map.set((nx as usize, ny as usize), c);
                 nx -= dx;
                 ny -= dy;
             }
-            set_tile(&mut grid, x, y, '.');
+            map.set((x as usize, y as usize), '.');
             x += dx; y += dy;
         }
     }
     let mut total = 0;
 
-    let (w, h) = data.size();
+    let (w, h) = puzzle.map.size();
     for y in 1..(h - 1) {
         for x in 1..(w - 1) {
-            if get_tile(&grid, x, y) == 'O' {
+            if map.get((x, y)) == 'O' {
                 total += x + 100 * y;
             }
         }
@@ -91,26 +64,15 @@ fn part_1(data: &Data) -> u32 {
     total as u32
 }
 
-fn part_2(data: &Data) -> u32 {
+fn part_2(data: &Puzzle) -> u32 {
     todo!("part 2");
-    // let grid: Vec<Vec<char>> = data.grid.iter().map(|cs| {
-    //     cs.iter().map(|c| {
-    //         match c {
-    //             '@' => "@.",
-    //             '#' => "##",
-    //             'O' => "[]",
-    //             '.' => "..",
-    //             _ => panic!("invalid tile")
-    //         }
-    //     }).collect()
-    // }).map(|s: String| s.chars().collect()).collect();
 }
 
 pub(crate) fn solve() {
     let data = include_str!("../../data/day_15/input.txt");
-    let data = Data::parse(data);
-    println!("part 1: {}", part_1(&data));
-    println!("part 2: {}", part_2(&data));
+    let puzzle = Puzzle::load(data);
+    println!("part 1: {}", part_1(&puzzle));
+    println!("part 2: {}", part_2(&puzzle));
 }
 
 #[cfg(test)]
@@ -118,38 +80,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_data() {
-        let data = include_str!("../../data/day_15/test_1.txt");
-        let data = Data::parse(data);
-        assert_eq!(data.grid.len(), 8);
-        assert_eq!(data.grid[0].len(), 8);
-        assert_eq!(data.moves.len(), 15);
-
-        let data = include_str!("../../data/day_15/test_2.txt");
-        let data = Data::parse(data);
-        assert_eq!(data.grid.len(), 10);
-        assert_eq!(data.grid[0].len(), 10);
-        assert_eq!(data.moves.len(), 700);
-    }
-
-    #[test]
     fn test_part_1_1() {
         let data = include_str!("../../data/day_15/test_1.txt");
-        let data = Data::parse(data);
-        assert_eq!(part_1(&data), 2028);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_1(&puzzle), 2028);
     }
 
     #[test]
     fn test_part_1_2() {
         let data = include_str!("../../data/day_15/test_2.txt");
-        let data = Data::parse(data);
-        assert_eq!(part_1(&data), 10092);
-    }
-
-    #[test]
-    fn test_part_2() {
-        let data = include_str!("../../data/day_15/test_1.txt");
-        let data = Data::parse(data);
-        assert_eq!(part_2(&data), 0);
+        let puzzle = Puzzle::load(data);
+        assert_eq!(part_1(&puzzle), 10092);
     }
 }
