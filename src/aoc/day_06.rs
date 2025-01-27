f5d3f15aa9febd09325e0f7cd4c87794a772e72d
dropfred <dropfred@github.com>
2025-01-27 09:44:13 +0100

    day 6 cleanup

--- src/aoc/day_06.rs
+++ src/aoc/day_06.rs
@@ -1,4 +1,6 @@
-#[derive(Eq, PartialEq, Debug)]
+use crate::aoc::grid::Grid;
+
+#[derive(Eq, PartialEq)]
 enum Step {
     New,
     Visited,
@@ -8,24 +10,19 @@ enum Step {
 
 type Position = (i32, i32, char);
 
-#[derive(Clone, Debug)]
-struct Data {
-    map: Vec<Vec<char>>,
+#[derive(Clone)]
+struct Puzzle {
+    map: Grid<char>,
     position: Option<Position>
 }
 
-impl Data {
+impl Puzzle {
     fn new(data: &str) -> Self {
         let map = data.trim().lines().map(|line| line.trim().chars().collect::<Vec<_>>()).collect::<Vec<_>>();
-        // let size = (map[0].len() as i32, map.len() as i32);
-        let mut position = None;
-        for (y, we) in map.iter().enumerate() {
-            if let Some(x) = we.iter().position(|c| "^v<>".contains(*c)) {
-                position = Some((x as i32, y as i32, we[x]));
-                break;
-            }
-        }
-        Data {map, position}
+        let map = Grid::from_vec(&map);
+        let position = map.find_by(|c| "^v<>".contains(*c));
+        let position = position.and_then(|(x, y)| Some((x as i32, y as i32, map.get((x, y)))));
+        Self {map, position}
     }
 
     fn next(&self) -> Option<Position> {
@@ -35,19 +32,18 @@ impl Data {
                 '>' => (x + 1, y),
                 'v' => (x, y + 1),
                 '<' => (x - 1, y),
-                // 'O' => (x, y),
                 _ => panic!("invalid direction")
             };
-            let (sx, sy) = (self.map[0].len() as i32, self.map.len() as i32);
+            let (sx, sy) = self.map.size();
+            let (sx, sy) = (sx as i32, sy as i32);
             if (nx >= 0) && (nx < sx) && (ny >= 0) && (ny < sy) {
-                let c = self.map[ny as usize][nx as usize];
+                let c = self.map.get((nx as usize, ny as usize));
                 if c == '#' {
                     let nd = match d {
                         '^' => '>',
                         '>' => 'v',
                         'v' => '<',
                         '<' => '^',
-                        // 'O' => 'O',
                         _ => panic!("invalid direction")
                     };
                     Some((x, y, nd))
@@ -65,8 +61,15 @@ impl Data {
         match position {
             Some((nx, ny, nd)) => {
                 self.position = position;
-                let c = &mut self.map[ny as usize][nx as usize];
-                if *c == '.' {*c = nd; Step::New} else if *c == nd {Step::Loop} else {Step::Visited}
+                let c = self.map.get((nx as usize, ny as usize));
+                if c == '.' {
+                    self.map.set((nx as usize, ny as usize), nd);
+                    Step::New
+                } else if c == nd {
+                    Step::Loop
+                } else {
+                    Step::Visited
+                }
             },
             None => {
                 self.position = None;
@@ -76,35 +79,37 @@ impl Data {
     }
 
     fn reset(&mut self, position: Option<Position>) {
-        for cs in &mut self.map {
-            for c in cs {
-                if *c != '#' {
-                    *c = '.';
+        let (w, h) = self.map.size();
+        for y in 0..h {
+            for x in 0..w {
+                let p = (x, y);
+                if self.map.get(p) != '#' {
+                    self.map.set(p, '.');
                 }
             }
         }
         if let Some((x, y, d)) = position {
-            self.map[y as usize][x as usize] = d;
+            self.map.set((x as usize, y as usize), d);
         }
         self.position = position;
     }
 }
 
-fn part_1(data: &mut Data) -> u32 {
+fn part_1(puzzle: &mut Puzzle) -> u32 {
     let mut total = 1u32;
-    while !data.position.is_none() {
-        if data.step() == Step::New {total += 1;}
+    while !puzzle.position.is_none() {
+        if puzzle.step() == Step::New {total += 1;}
     }
     total
 }
 
-fn part_2(data: &mut Data) -> u32 {
+fn part_2(puzzle: &mut Puzzle) -> u32 {
     let mut total = 0;
-    while !data.position.is_none() {
-        let mut test = data.clone();
+    while !puzzle.position.is_none() {
+        let mut test = puzzle.clone();
         if let Some((x, y, _)) = test.next() {
-            if test.map[y as usize][x as usize] == '.' {
-                test.map[y as usize][x as usize] = '#';
+            if test.map.get((x as usize, y as usize)) == '.' {
+                test.map.set((x as usize, y as usize), '#');
                 while !test.position.is_none() {
                     if test.step() == Step::Loop {
                         total += 1;
@@ -113,18 +118,18 @@ fn part_2(data: &mut Data) -> u32 {
                 }
             }
         }
-        data.step();
+        puzzle.step();
     }
     total
 }
 
 pub(crate) fn solve() {
     let data = include_str!("../../data/day_06/input.txt");
-    let mut data = Data::new(data);
-    let start = data.position;
-    println!("part 1: {}", part_1(&mut data));
-    data.reset(start);
-    println!("part 2: {}", part_2(&mut data));
+    let mut puzzle = Puzzle::new(data);
+    let start = puzzle.position;
+    println!("part 1: {}", part_1(&mut puzzle));
+    puzzle.reset(start);
+    println!("part 2: {}", part_2(&mut puzzle));
 }
 
 mod tests {
@@ -133,21 +138,14 @@ mod tests {
     const DATA: &str = include_str!("../../data/day_06/test.txt");
 
     #[test]
-    fn test_data() {
-        let data = Data::new(DATA);
-        assert_eq!(data.map.len(), 10);
-        assert!(data.map.iter().all(|line| line.len() == 10));
-    }
-
-    #[test]
     fn test_part_1() {
-        let mut data = Data::new(DATA);
-        assert_eq!(part_1(&mut data), 41);
+        let mut puzzle = Puzzle::new(DATA);
+        assert_eq!(part_1(&mut puzzle), 41);
     }
 
     #[test]
     fn test_part_2() {
-        let mut data = Data::new(DATA);
-        assert_eq!(part_2(&mut data), 6);
+        let mut puzzle = Puzzle::new(DATA);
+        assert_eq!(part_2(&mut puzzle), 6);
     }
 }
